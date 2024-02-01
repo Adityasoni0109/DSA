da7ea5e910b557e9ac19fda6d474310a73740527
ADITYA KUMAR SONI <77963715+Adityasoni0109@users.noreply.github.com>
2024-02-01 21:57:32 +0530

    Add files via upload

---
+++ String/stringg.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    string aditya="helolololo";
+    cout<<aditya.length();
+    cout<<aditya[0];
+    cout<<aditya[1];
+    cout<<aditya[2];
+ 
+ return 0;
+}
